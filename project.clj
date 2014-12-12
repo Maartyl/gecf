0afd660efbbb0554db924465a2c86080fe97fe23
Maartyl <kuronomaartyl@gmail.com>
2014-12-12 15:04:46 +0100

    simple description in projet.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gecf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "computes k-edge-conectedness of a graph computing flow, preflow-relabel"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
